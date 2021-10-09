99d2c1dee422d8909f5889b0f03b1916281fcba0
Justin Oeters <justin.oeters@gmail.com>
2021-10-09 16:41:52 -0400

    finished initial schema format

--- schema.sql
+++ schema.sql
@@ -6,4 +6,27 @@ CREATE DATABASE employee_trackerDB;
 USE employee_trackerDB;
 
 -- Department Table
+CREATE TABLE department (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    NAME VARCHAR(30)
+    );
 
+-- Employee Role Table
+CREATE TABLE role (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+);
+
+-- Employee Table
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+    FOREIGN KEY (manager_id) REFERENCES manager(id) ON DELETE CASCADE
+);
